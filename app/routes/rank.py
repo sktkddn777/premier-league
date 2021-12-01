723210361becb40b9391787dea19393d865d79b9
sktkddn777 <sktkddn777@naver.com>
2021-12-01 13:58:05 +0900

    add rank route

--- app/routes/rank.py
+++ app/routes/rank.py
@@ -1,18 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import json
 
-
-from database import (
-    retrieve_teams_ranking,
-)
-from models.teams import (
-    ResponseModel,
-)
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+def get_icon(name):
+    with open('../football_json/test.json', 'r') as f:
+        data = json.load(f)
+    return data[name]
+
+# # helper
+def rank_helper(team) -> dict:
+  return {
+      "icon": get_icon(team["team_name"]),
+      "name": team["team_name"],
+      "points": team["points"],
+  }
 
 @router.get("/", response_description="Teams ranking data retrieved")
-async def retrieve_teams_data():
-    teams = await retrieve_teams_ranking()
-    if teams:
-        return ResponseModel(teams, "Teams data retrieved successfully")
-    return ResponseModel(teams, "Empty")
+async def read_item(request: Request):
+  team = []
+  
+  with open('../football_json/Premier League.json', 'r') as f:
+    data = json.load(f)
+
+  for t in data['Premier League']:
+    team.append(rank_helper(t))
+  return templates.TemplateResponse("index.html", {"request": request, "data":team})
